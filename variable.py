5a6a7e7e99f5a0c00a7a00bc9ac9e09b4a457eed
Faruque5698 <ashaduzzaman5698@gmail.com>
2024-05-15 10:57:50 +0600

    python variable

---
+++ variable.py
@@ -0,0 +1,34 @@
+# x = 5 #int type
+# y = "john" #string type
+# print(type(x))
+# print(type(y))
+#print(x)
+#print(y)
+
+#x = 5
+#x = "Salary"
+#print(x)
+
+#x = str(3) #string
+#y = int(3) #int
+#z = float(3) #float
+
+#print(x)
+#print(y)
+#print(z)
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)

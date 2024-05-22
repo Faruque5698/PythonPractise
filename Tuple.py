c08f95de529e2cf1f25f1aa4d61633455298bcf8
Faruque5698 <ashaduzzaman5698@gmail.com>
2024-05-22 18:21:48 +0600

    python

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+# mytuple = ("apple", "banana", "cherry")
+# print(len(mytuple))
+
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+# thistuple = tuple(("apple", "banana", "cherry"))
+# print(type(thistuple))
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
---
+++ condition.py
@@ -0,0 +1,19 @@
+# a = int(input("Enter number a:\n"))
+# b = int(input("Enter number b:\n"))
+# if a > b:
+#     print(f"maximum: {a}")
+# else:
+#     print(f"maximum: {b}")
+
+
+
+# a = int(input("Enter a number a: \n"))
+# b = int(input("Enter a number b: \n"))
+# c = int(input("Enter a number c: \n"))
+#
+# if a > b and a > c:
+#     print(f"maximum is {a}")
+# elif b > a and b > c:
+#     print(f"maximum is {b}")
+# else:
+#     print(f"maximum is {c}")

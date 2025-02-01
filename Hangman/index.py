e18638ba5cca8c64f57dc11e529c2b6c5252d773
Md. Ashaduzzaman Faruque <Ashaduzzaman5698@gmail.com>
2025-02-01 12:56:17 +0600

    Hangman game

--- Hangman/index.py
+++ Hangman/index.py
@@ -1,15 +1,47 @@
 import random
+import hangman_words
+import hangman_arts
 
-word_list = ['aardvark', 'baboon', 'camel']
-# word_list_len = len(word_list)
-# print(word_list[random.randint(0,word_list_len-1)])
-randomWord = random.choice(word_list)
-print(randomWord)
-guess_word = input('Guess a letter: ').lower()
+lives = 6
+stages = hangman_arts.stages
+
+print(hangman_arts.logo)
+
+randomWord = random.choice(hangman_words.word_lists)
+
+placeHolder = ""
+
+# print("This is the letter: ")
+
 for words in randomWord:
-    if guess_word == words:
-        print('Right')
-    else:
-        print('Wrong')
+    placeHolder += '_ '
+print(placeHolder)
 
+game_over = False
+correct_letters = []
 
+while not game_over:
+    guess_word = input('\nGuess a letter: ').lower()
+    display = ""
+
+    for words in randomWord:
+        if guess_word == words:
+            display += words
+            correct_letters.append(words)
+        elif words in correct_letters:
+            display += words
+        else:
+            display += "_ "
+
+    print(display)
+
+    if guess_word not in randomWord:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print('You lose')
+
+    if "_ " not in display:
+        game_over = True
+        print("You won")
+    print(stages[lives])
